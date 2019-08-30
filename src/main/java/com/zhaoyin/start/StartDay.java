2d57390cb206f465f0c4f618757661c9a05f2db6
godspysonyou <1344094556@qq.com>
2019-08-30 16:49:25 +0800

    # Flink config start

---
+++ src/main/java/com/zhaoyin/start/StartDay.java
@@ -0,0 +1,13 @@
+package com.zhaoyin.start;
+
+import java.util.Date;
+
+/**
+ * Now start the Flink configuration
+ * @author ckm
+ */
+public class StartDay {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+    }
+}
